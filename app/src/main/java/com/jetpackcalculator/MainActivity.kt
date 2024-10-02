1bd27df9f926cd19d92d8bee010e5d8fa093f632
SagarKapase <sagarkapase139@gmail.com>
2024-10-02 19:11:10 +0530

    Created UI

--- app/src/main/java/com/jetpackcalculator/MainActivity.kt
+++ app/src/main/java/com/jetpackcalculator/MainActivity.kt
@@ -20,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackCalculatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Calculator(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackCalculatorTheme {
-        Greeting("Android")
     }
 }
